c7936d66839a65d5458560418141d69012a101a9
DESKTOP-APK21IT\itoka229 <itoka229@yahoo.co.jp>
2019-02-21 13:44:00 +0900

    No.2

---
+++ Mine Craft/Assets/Scripts/Ray.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ray : MonoBehaviour
+{
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity))
+        {
+            var point = hit.point;
+            Debug.Log(point);
+        }
+    }
+}
