a0095804741ecec7c0dc06b76cc801646719628c
kumachan0210 <kumachan0220ios@gmail.com>
2019-02-21 12:17:37 +0900

    No.1

---
+++ Mine Craft/Assets/Scripts/CreateWorld.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateWorld : MonoBehaviour
+{
+
+    public GameObject prefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+        for(int i = -5; i < 5; i++)
+        {
+            for(int j = -5; j < 5; j++)
+            {
+                Instantiate(prefab,new Vector3(i,0,j) , Quaternion.identity);
+            }
+        }
+    }
+
+    
+}
